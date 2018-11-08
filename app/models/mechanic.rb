fe89aa010f2ba5fc3d54d8914779b6ad3ad302cf
artismarti <artismarti@gmail.com>
2018-11-08 16:38:38 +0000

    Updates Mechanic Class

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,27 @@
 class Mechanic
 
   attr_reader :name, :specialty
+  @@all = []
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all
+    @@all
+  end
 
+  def cars_serviced
+    Car.all.select{|c| c.mechanic == self}
+  end
+
+  def car_owners
+    cars_serviced.map{|c| c.car_owner}.uniq
+  end
+
+  def car_owner_names
+    car_owners.map{|co| co.name}
+  end
 
 end
